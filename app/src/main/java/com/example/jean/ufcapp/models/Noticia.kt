e6b4dbd7df35fdbc8477e7db0627c81299c5575b
jean <jeancs.patricio@gmail.com>
2018-12-13 12:00:50 -0300

    Criando modelo base.

---
+++ app/src/main/java/com/example/jean/ufcapp/models/Noticia.kt
@@ -0,0 +1,13 @@
+package com.example.jean.ufcapp.models
+
+/**
+ * @author Jean Carlos (Github: @jeancsanchez)
+ * @date 13/12/18.
+ * Jesus is alive!
+ */
+data class Noticia(
+        val id: Long,
+        val autor: String,
+        val titulo: String,
+        val foto: String
+)
