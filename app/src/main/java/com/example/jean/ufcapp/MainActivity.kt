e0ed1a02a6908bffdda95b056b7be1837f02adcd
jean <jeancs.patricio@gmail.com>
2018-12-13 12:07:32 -0300

    Integrando presenter com a view.

--- app/src/main/java/com/example/jean/ufcapp/MainActivity.kt
+++ app/src/main/java/com/example/jean/ufcapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.jean.ufcapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.example.jean.ufcapp.models.Noticia
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +12,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        presenter.attach(this)
         presenter.carregarNoticias()
+    }
+
+
+    fun mostrarNoticias(list: List<Noticia>) {
+
+    }
+
+    fun mostrarNoticia(noticia: Noticia) {
+
     }
 }
--- app/src/main/java/com/example/jean/ufcapp/MainPresenter.kt
+++ app/src/main/java/com/example/jean/ufcapp/MainPresenter.kt
@@ -1,17 +1,35 @@
 package com.example.jean.ufcapp
 
+import com.example.jean.ufcapp.models.Noticia
+
 /**
  * @author Jean Carlos (Github: @jeancsanchez)
  * @date 13/12/18.
  * Jesus is alive!
  */
 class MainPresenter {
+    var activity: MainActivity? = null
+    private val fakeNoticia = Noticia(
+            id = 1,
+            autor = "Jean",
+            titulo = "Lutador quebra pescoço de adversário",
+            foto = "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252F1%252F230%252F3GettyImages-1056094360.jpg?mw300-mh300-tc1"
+    )
+
+    fun attach(activity: MainActivity) {
+        this.activity = activity
+    }
 
     fun carregarNoticias(id: Int? = null) {
         id?.let {
-            // Carregar noticia
+            activity?.mostrarNoticia(fakeNoticia)
+
         } ?: let {
-            // Carregar Noticias
+            val noticias = listOf(
+                    fakeNoticia,
+                    fakeNoticia.copy(id = 2, titulo = "Quebra pau no palco!", foto = "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252F1%252F230%252F044148_230_WeighIn_ENG_669x376.jpg?mw300-mh300-tc1")
+            )
+            activity?.mostrarNoticias(noticias)
         }
     }
 }
